8f9203668a7b22946825c85376983355284ad2d5
Ștefan Țîncu <stefantnc11@gmail.com>
2022-05-06 11:21:47 +0300

    Use native dialog options

--- packages/renderer/src/components/toolbar/toolbar.tsx
+++ packages/renderer/src/components/toolbar/toolbar.tsx
@@ -1,11 +1,7 @@
-import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import React, { forwardRef, useImperativeHandle } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToolbarIcons } from '@utils/icons';
 import ReactTooltip from 'react-tooltip';
-import Modal from 'react-modal';
-import { MessageBox } from '../messagebox/messagebox';
-import { DialogBox } from '../dialogbox/dialogbox';
-import { dialogStyles } from '@src/styles/styles';
 import { routes } from '@src/router/routes';
 import { deleteAlbum } from '@src/services/album-services';
 
@@ -17,43 +13,10 @@ type ToolbarProps = {
 
 const Toolbar = forwardRef((props: ToolbarProps, ref?: any) => {
     const navigate = useNavigate();
-    const [messageOpen, setMessageOpen] = useState(false);
-    const [messageboxMsg, setMessageboxMsg] = useState('');
-    const [dialogbox, setDialogbox] = useState(false);
-    const [accept, setAccept] = useState(false);
 
     useImperativeHandle(ref, () => ({
         //
     }));
-
-    useEffect(() => {
-        if (accept == true) {
-            requestResourceDeletion();
-            //Reset action accept state
-            setAccept(false);
-        }
-    }, [accept]);
-
-    async function requestResourceDeletion(): Promise<void> {
-        //Activate loading screen
-        try {
-            if (props.item !== undefined && props.itemId !== undefined) {
-                //Make an array with all filenames under the album
-                const filesToDelete = [];
-                for (const song of props.item.songs) {
-                    filesToDelete.push(`${song.name}.${song.extension}`);
-                }
-                filesToDelete.push(`artwork.${props.item.extension}`);
-                //Delete album from storage and database
-                await deleteAlbum(props.itemId, { album: props.itemId, files: filesToDelete });
-                navigate('/album/view/0');
-            }
-        } catch (error: any) {
-            //Set message and notify user about occured error
-            setMessageboxMsg(error.message);
-            setMessageOpen(true);
-        }
-    }
 
     function onReviewClick(): void {
         navigate(`/album/reviews/${props.itemId}`);
@@ -63,8 +26,31 @@ const Toolbar = forwardRef((props: ToolbarProps, ref?: any) => {
         navigate(`/album/edit/${props.itemId}`);
     }
 
-    function onRequestDeleteClick(): void {
-        setDialogbox(true);
+    async function onRequestDeleteClick(): Promise<void> {
+        // setDialogbox(true);
+        const clicked = await window.electron.showQuestionBox('', 'Are you sure you want to delete this item ?');
+        if (clicked) {
+            try {
+                //Activate loading screen
+                window.appendLoading();
+                if (props.item !== undefined && props.itemId !== undefined) {
+                    //Make an array with all filenames under the album
+                    const filesToDelete = [];
+                    for (const song of props.item.songs) {
+                        filesToDelete.push(`${song.name}.${song.extension}`);
+                    }
+                    filesToDelete.push(`artwork.${props.item.extension}`);
+                    //Delete album from storage and database
+                    await deleteAlbum(props.itemId, { album: props.itemId, files: filesToDelete });
+                    window.removeLoading();
+                    navigate('/album/view/0');
+                }
+            } catch (error: any) {
+                //Set message and notify user about occured error
+                window.removeLoading();
+                window.electron.showErrorMessage(error.message);
+            }
+        }
     }
 
     return (
@@ -86,16 +72,6 @@ const Toolbar = forwardRef((props: ToolbarProps, ref?: any) => {
                 <img src={ToolbarIcons.Reviews} className="ActionIcon" />
                 <p>Reviews</p>
             </div>
-            <Modal isOpen={messageOpen} style={dialogStyles} contentLabel="Upload" ariaHideApp={false}>
-                <MessageBox setIsOpen={setMessageOpen} message={messageboxMsg} />
-            </Modal>
-            <Modal isOpen={dialogbox} style={dialogStyles} contentLabel="Dialog box" ariaHideApp={false}>
-                <DialogBox
-                    setIsOpen={setDialogbox}
-                    message="Are you sure you want to delete this item ?"
-                    setAccepted={setAccept}
-                />
-            </Modal>
         </div>
     );
 });
