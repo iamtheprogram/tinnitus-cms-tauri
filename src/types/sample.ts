527170047b8204853e461e933e32b7874d2a36f8
sinclair11 <stefantnc11@gmail.com>
2022-06-19 21:19:16 +0300

    Add  new type for sample data
    Resolves: #7

---
+++ src/types/sample.ts
@@ -0,0 +1,13 @@
+export type SampleFormData = {
+    name: string;
+    file?: string;
+    description: string;
+    extension?: string;
+    tags?: string[];
+    length: string;
+    category: string;
+    notification?: string;
+    likes?: number;
+    favorites?: number;
+    views?: number;
+};
