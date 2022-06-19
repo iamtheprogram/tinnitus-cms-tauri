155f28f55c2394a8e5e99ba4141086e5ec5ad3e5
sinclair11 <stefantnc11@gmail.com>
2022-06-19 21:34:44 +0300

    Add new type for retrived sample data from db
    Resolves: #7

--- src/types/sample.ts
+++ src/types/sample.ts
@@ -11,3 +11,18 @@ export type SampleFormData = {
     favorites?: number;
     views?: number;
 };
+
+export type SampleInfo = {
+    id: string;
+    name: string;
+    description: string;
+    extension: string;
+    tags: string[];
+    length: string;
+    category: string;
+    likes: number;
+    reviews: number;
+    upload_date: any;
+    favorites: number;
+    views: number;
+};
